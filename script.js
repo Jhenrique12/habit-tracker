a6f775801e565468dbeea1d14f92fed6a6b31d6d
Jhenrique12 <jonhenrique007@gmail.com>
2023-01-19 11:23:21 -0300

    Add: call NLW library and test w/ data content

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form) 
 
 const data = {
-  run: ['01-01', '01-02', '01-06'],
+  run: ['01-01', '01-02', '01-06', '01-07', '01-08'],
   study: ['01-02'],
-  programming: ['02-01','02-02','02-03']
+  programming: ['01-06', '01-07', '01-08','02-01','02-02','02-03']
 }
 
 nlwSetup.setData(data)
